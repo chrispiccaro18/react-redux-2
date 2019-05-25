8f17f52a83b8b0f86b17c31a6435e5b4604549c6
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-25 09:16:48 -0700

    delete comment action

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,11 +1,19 @@
 const ADD_COMMENT = 'ADD_COMMENT';
+const DELETE_COMMENT = 'DELETE_COMMENT';
 
 const addComment = body => ({
   type: ADD_COMMENT,
   payload: body
 });
 
+const deleteComment = commentId => ({
+  type: DELETE_COMMENT,
+  payload: commentId
+});
+
 export {
   ADD_COMMENT,
-  addComment
+  DELETE_COMMENT,
+  addComment,
+  deleteComment
 };
--- src/actions/commentActions.test.js
+++ src/actions/commentActions.test.js
@@ -1,10 +1,17 @@
-import { addComment, ADD_COMMENT } from './commentActions';
+import { addComment, ADD_COMMENT, deleteComment, DELETE_COMMENT } from './commentActions';
 
 describe('comment actions', () => {
   it('can add a comment', () => {
     expect(addComment('test comment')).toEqual({
       type: ADD_COMMENT,
       payload: 'test comment'
+    });
+  });
+
+  it('can delete a comment', () => {
+    expect(deleteComment('001')).toEqual({
+      type: DELETE_COMMENT,
+      payload: '001'
     });
   });
 });
