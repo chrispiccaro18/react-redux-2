ecd004827db696fc940613b20ef47772a744b57d
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 10:59:39 -0700

    add router to app for post link

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Post from './posts/Post';
 
 export default function App() {
   const post = { id: '22', title: 'hi', body: 'there' };
   return (
-    <ul>
-      <Post post={post}></Post>
-    </ul>
+    <Router>
+      <ul>
+        <Post post={post}></Post>
+      </ul>
+    </Router>
   );
 }
