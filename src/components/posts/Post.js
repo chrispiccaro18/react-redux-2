f88985c925a2931b448fc917590a0f4451795183
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 10:37:35 -0700

    refactored post component

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Post({ post }) {
-  const { title, id } = post;
+  const { id, title } = post;
   return (
     <Link to={`/post/${id}`}>
       <li>{title}</li>
@@ -13,8 +13,8 @@ function Post({ post }) {
 
 Post.propTypes = {
   post: PropTypes.shape({
-    title: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     body: PropTypes.string
   }).isRequired,
 };
