d35699aeb1df32cdfa86aff833793e125928c4b7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 11:04:22 -0700

    posts component

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Post from './Post';
+
+function Posts({ posts }) {
+  const postLis = posts.map(post => (
+    <Post key={post.id} post={post}/>
+  ));
+  return (
+    <ul>
+      {postLis}
+    </ul>
+  );
+}
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string
+    })
+  ).isRequired
+};
+
+export default Posts;
