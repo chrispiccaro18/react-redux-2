81e6d311f7215ca96852bbda49db2ab1695d2d03
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 11:34:58 -0700

    All Posts container

---
+++ src/containers/AllPosts.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import Posts from '../components/posts/Posts';
+import { getPosts } from '../selectors/postSelectors';
+
+const mapStateToProps = state => ({
+  posts: getPosts(state)
+});
+
+export default connect(
+  mapStateToProps
+)(Posts);
