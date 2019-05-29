c4de57afac1d64ff4afea9fa0d80be6e0141d747
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 11:24:40 -0700

    form rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Posts from './posts/Posts';
+import PostForm from './posts/PostForm';
 
 export default function App() {
   const posts = [
@@ -8,8 +9,10 @@ export default function App() {
     { id: '002', title: 'title2', body: 'body2' },
     { id: '003', title: 'title3', body: 'body3' }
   ];
+  const postOnSubmit = (title, body) => console.log(title, body);
   return (
     <Router>
+      <PostForm postOnSubmit={postOnSubmit} />
       <Posts posts={posts} />
     </Router>
   );
