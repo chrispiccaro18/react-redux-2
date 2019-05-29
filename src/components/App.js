04739601398ec3daf6166e30bda6fc31fbb998fd
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 11:35:32 -0700

    Moved containers into home

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Posts from './posts/Posts';
-import PostForm from './posts/PostForm';
+import Home from './Home';
 
 export default function App() {
-  const posts = [
-    { id: '001', title: 'title1', body: 'body1' },
-    { id: '002', title: 'title2', body: 'body2' },
-    { id: '003', title: 'title3', body: 'body3' }
-  ];
-  const postOnSubmit = (title, body) => console.log(title, body);
   return (
     <Router>
-      <PostForm postOnSubmit={postOnSubmit} />
-      <Posts posts={posts} />
+      <Home />
     </Router>
   );
 }
