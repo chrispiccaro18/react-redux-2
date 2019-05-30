ba91153be27ecf77faa8abd7afa90eb069a1720e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 10:51:45 -0700

    comments component

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+
+function Comments({ comments }) {
+  const commentLis = comments.map(comment => (
+    <Comment key={comment.id} comment={comment.commentBody} />
+  ));
+  return (
+    <ul>
+      {commentLis}
+    </ul>
+  );
+}
+
+Comments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    commentBody: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Comments;
